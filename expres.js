1a356dcc8520c71ee9d8c7930d6a72bf360dfae1
mian anees <miananees23@gmail.com>
2019-11-12 12:15:22 +0500

    comit1.1

--- expres.js
+++ expres.js
@@ -15,9 +15,11 @@ app.get('/',(req,res)=>{
     res.send('done').json('done');
     
     
-});
-app.get('/api/:id/:id2',async (req,res)=>{
- const id1=req.params.id;
+   });
+app.get('/api/:id/:id2',async (req,res,next)=>{
+
+ try {
+    const id1=req.params.id;
  const id2=req.params.id2;
     apiclass.signup();
    console.log(id1,id2);
@@ -27,11 +29,17 @@ app.get('/api/:id/:id2',async (req,res)=>{
    });
    const mdbres = await useer.save().then(res=>{
        console.log(res);
-       console.log(mdbres);
+       
        console.log('ok');
    });
+   console.log(mdbres);
+  } 
+ catch (err){
+          next(err);
+            }
+  
    console.log('ok2');
    
    res.send('ok done');   
-})
+});
 app.listen(port,()=>{console.log('awaiting ')});
